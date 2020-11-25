7870797f95745032503e2741179821c61cda1f88
huangfusuper <huangfusuper@163.com>
2020-11-25 11:43:00 +0800

    main分支提交

--- src/main/java/com/test/git/MainTestGit.java
+++ src/main/java/com/test/git/MainTestGit.java
@@ -3,5 +3,9 @@ package com.test.git;
 public class MainTestGit {
     public static void main(String[] args) {
         System.out.println("new Git");
+        System.out.println("mian");
+        System.out.println("mian");
+        System.out.println("mian");
+        System.out.println("mian");
     }
 }
--- src/main/java/com/test/git/MainTestGit2.java
+++ src/main/java/com/test/git/MainTestGit2.java
@@ -3,5 +3,9 @@ package com.test.git;
 public class MainTestGit2 {
     public static void main(String[] args) {
         System.out.println("new Gi2222t");
+        System.out.println("mian");
+        System.out.println("mian");
+        System.out.println("mian");
+        System.out.println("mian");
     }
 }
