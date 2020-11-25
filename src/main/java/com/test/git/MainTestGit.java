df4dae2221daa0746ab9c88024b1ebbbde1ea192
huangfusuper <huangfusuper@163.com>
2020-11-25 11:42:16 +0800

    dev分支提交

--- src/main/java/com/test/git/MainTestGit.java
+++ src/main/java/com/test/git/MainTestGit.java
@@ -3,5 +3,12 @@ package com.test.git;
 public class MainTestGit {
     public static void main(String[] args) {
         System.out.println("new Git");
+        System.out.println("new Git");
+        System.out.println("new Git");
+        System.out.println("new Git");
+        System.out.println("new Git");
+        System.out.println("new Git");
+        System.out.println("new Git");
+        System.out.println("new Git");
     }
 }
